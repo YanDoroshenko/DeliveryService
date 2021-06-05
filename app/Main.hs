e923091ff496c971a732e278ecb7ee9eba629901
Yan Doroshenko <yan1994doroshenko@gmail.com>
2021-06-06 00:08:08 +0200

    [new] Scotty POC

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 
-import Data.Int
-import Control.Monad.IO.Class
-import Data.Text (Text)
-import Data.Text
-import Data.Text.Lazy
-import Data.Functor.Identity
-import Database.CQL.IO as Client
-import qualified System.Logger as Logger
-import Database.CQL.Protocol
-
-connect:: IO ClientState
-connect = do
-    g <- Logger.new Logger.defSettings
-    c <- Client.init defSettings
-    return c
-
-cqlParams :: Tuple a => a -> QueryParams a
-cqlParams p = QueryParams One False p Nothing Nothing Nothing (Just True)
-
-cqlQuery queryString p client  =
-    runClient client $ query queryString $ cqlParams p
-
-getCustomerIds db =
-    cqlQuery ((QueryString $ fromStrict $ Data.Text.pack "SELECT id, name from user.users;") :: QueryString R() (Int32, Data.Text.Text)) () db
+import Web.Scotty
+import Data.Text.Lazy.Encoding (decodeUtf8)
 
 main :: IO ()
-main =
-  do
-    c <- liftIO connect
-    ids <- getCustomerIds c
-    putStrLn $ Prelude.foldr (\x y -> (show $ fst x) ++ " " ++ (Data.Text.unpack (snd x)) ++ "\n" ++ y) "" ids
+main = scotty 3000 $
+    get "/:str" $ do
+        str <- param "str"
+        json (str :: String)
