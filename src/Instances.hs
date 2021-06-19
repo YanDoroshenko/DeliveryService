d5a4d4f0a30d72c8ceeff35d2a90eca7a0ccc8d7
Yan Doroshenko <yan1994doroshenko@gmail.com>
2021-06-19 14:44:24 +0200

    [upd] Refactoring

--- src/Instances.hs
+++ src/Instances.hs
@@ -20,31 +20,21 @@ instance FromJSON Rate where
     fromMaybe (parseBaseDistanceRate v) $ ((\_ -> parsePostalCodeRate v) <$> p) `Utils.or` ((\_ -> parseLocationRate v) <$> l) `Utils.or` ((\_ -> parseStateRate v) <$> s)
   parseJSON _ = empty
 
+withId x y = y <$> (x .: "id")
+
+withRate x y = y <*> (x .: "rate")
 
 parsePostalCodeRate :: Object -> Parser Rate
-parsePostalCodeRate = \v -> PostalCodeOverrideRate
-      <$> v .: "id"
-      <*> v .: "postalCode"
-      <*> v .: "rate"
+parsePostalCodeRate v = withRate v $ (withId v PostalCodeOverrideRate) <*> v .: "postalCode"
 
 parseLocationRate :: Object -> Parser Rate
-parseLocationRate  = \v -> LocationOverrideRate
-      <$> v .: "id"
-      <*> v .: "locationId"
-      <*> v .: "rate"
+parseLocationRate v = withRate v $ (withId v LocationOverrideRate) <*> v .: "locationId"
 
 parseBaseDistanceRate :: Object -> Parser Rate
-parseBaseDistanceRate = \v -> BaseDistanceRate
-  <$> v .: "id"
-  <*> v .:? "distanceFrom"
-  <*> v .:? "distanceTo"
-  <*> v .: "rate"
+parseBaseDistanceRate v = withRate v $ (withId v BaseDistanceRate) <*> v .:? "distanceFrom" <*> v .:? "distanceTo"
 
 parseStateRate :: Object -> Parser Rate
-parseStateRate = \v -> StateOverrideRate
-  <$> v .: "id"
-  <*> v .: "stateCode"
-  <*> v .: "rate"
+parseStateRate v = withRate v $ (withId v StateOverrideRate) <*> v .: "stateCode"
 
 instance ToJSON Rate
 
