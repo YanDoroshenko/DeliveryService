fd8c5e68fd871823c7199d877c796b9d6e9d9cf6
Yan Doroshenko <yan1994doroshenko@gmail.com>
2021-06-28 22:32:51 +0200

    [upd] Improve tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,9 +5,25 @@ import Test.QuickCheck
 
 main :: IO ()
 main = do
-  x <- generate generateStartingPrice
-  quickCheck $ (price 0 0 x) == (startingPrice x)
+  _ <- checkStartingPrice
+  checkSubtotalFactor
+
+checkStartingPrice = do
+  rate <- generate generateStartingPrice
+  subtotal <- generate arbitrary
+  weight <- generate arbitrary
+  quickCheck $ (price subtotal weight rate) == (startingPrice rate)
+
+checkSubtotalFactor = do
+  rate <- generate generateSubtotalFactor
+  subtotal <- generate arbitrary
+  weight <- generate arbitrary
+  quickCheck $ (price subtotal weight rate) == ((subtotal *) <$> (subtotalFactor rate))
 
 generateStartingPrice = do
   startingPrice <- arbitrary
   return $ RateDef startingPrice Nothing Nothing Nothing Nothing Nothing Nothing Nothing
+
+generateSubtotalFactor = do
+  subtotalFactor <- arbitrary
+  return $ RateDef Nothing subtotalFactor Nothing Nothing Nothing Nothing Nothing Nothing
