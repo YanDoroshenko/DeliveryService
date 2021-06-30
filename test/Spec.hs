c12b34fb1d92fa086bd07dc2dbead6045d5eea99
Yan Doroshenko <yan1994doroshenko@gmail.com>
2021-06-30 18:49:14 +0200

    [upd] Improve tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,22 @@
 import Model
 import Service
 
+import Data.Maybe
 import Test.QuickCheck
 
 main :: IO ()
 main = do
   _ <- checkStartingPrice
   _ <- checkSubtotalFactor
-  checkWeightFactor
+  _ <- checkWeightFactor
+  _ <- checkLowerSubtotalThreshold1
+  checkLowerSubtotalThreshold2
 
 checkStartingPrice = do
   rate <- generate generateStartingPrice
   subtotal <- generate arbitrary
   weight <- generate arbitrary
-  quickCheck $ (price subtotal weight rate) == (startingPrice rate)
+  verboseCheck $ (price subtotal weight rate) == (startingPrice rate)
 
 checkSubtotalFactor = do
   rate <- generate generateSubtotalFactor
@@ -27,6 +30,18 @@ checkWeightFactor = do
   weight <- generate arbitrary
   quickCheck $ (price subtotal weight rate) == (((\w -> (*) (fromIntegral $ ceiling $ weight / w)) <$> (weightInterval rate)) <*> (startingPrice rate))
 
+checkLowerSubtotalThreshold1 = do
+  rate <- generate generateLowerSubtotalThreshold
+  subtotal <- generate $ suchThat arbitrary (\x -> fromMaybe True $ (x >= ) <$> (lowerSubtotalThreshold rate))
+  weight <- generate arbitrary
+  quickCheck $ (price subtotal weight rate) == (startingPrice rate)
+
+checkLowerSubtotalThreshold2 = do
+  rate <- generate generateLowerSubtotalThreshold
+  subtotal <- generate $ suchThat arbitrary (\x -> fromMaybe False $ (x < ) <$> (lowerSubtotalThreshold rate))
+  weight <- generate arbitrary
+  quickCheck $ (price subtotal weight rate) == Nothing
+
 generateStartingPrice = do
   startingPrice <- arbitrary
   return $ RateDef startingPrice Nothing Nothing Nothing Nothing Nothing Nothing Nothing
@@ -39,3 +54,8 @@ generateWeightInterval = do
   startingPrice <- arbitrary
   weightInterval <- arbitrary
   return $ RateDef startingPrice Nothing Nothing Nothing Nothing Nothing Nothing weightInterval
+
+generateLowerSubtotalThreshold = do
+  startingPrice <- arbitrary
+  threshold <- arbitrary
+  return $ RateDef startingPrice Nothing threshold Nothing Nothing Nothing Nothing Nothing
